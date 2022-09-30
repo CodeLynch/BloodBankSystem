03ac189c900861801f80a35de5384b431ce40ebe
Faith Rosalijos <114710216+faithrosalijos@users.noreply.github.com>
2022-09-30 14:18:00 +0800

    Modify Models, Add Login/Logout, Add User Registration

--- BloodBankSystem/accounts/forms.py
+++ BloodBankSystem/accounts/forms.py
@@ -1,10 +1,94 @@
-from django.forms import ModelForm
 from django import forms
-
 from .models import *
 
 
-class IndividualForm(ModelForm):
+class DonorForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput()),
+    password = forms.CharField(widget=forms.PasswordInput()),
+    type = 'I'
+    first_name = forms.CharField(widget=forms.TextInput()),
+    middle_name = forms.CharField(widget=forms.TextInput()),
+    last_name = forms.CharField(widget=forms.TextInput()),
+    contact_number = forms.CharField(widget=forms.TextInput()),
+    age = forms.IntegerField(widget=forms.NumberInput()),
+    weight = forms.FloatField(widget=forms.NumberInput()),
+    blood_type = forms.CharField(widget=forms.Select()),
+    health_condition = forms.CharField(widget=forms.Textarea()),
+    individual_type = 'D'
+
+    class Meta:
+        model = Donor
+        fields = ('username', 'password', 'first_name', 'middle_name', 'last_name', 'contact_number', 'age', 'weight', 'blood_type', 'health_condition')
+
+    def __init__(self, *args, **kwargs):
+        super(DonorForm, self).__init__(*args, **kwargs)
+        self.instance.type = self.type
+        self.instance.individual_type = self.individual_type
+
+
+class RecipientForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput()),
+    password = forms.CharField(widget=forms.PasswordInput()),
+    type = 'I'
+    first_name = forms.CharField(widget=forms.TextInput()),
+    middle_name = forms.CharField(widget=forms.TextInput()),
+    last_name = forms.CharField(widget=forms.TextInput()),
+    contact_number = forms.CharField(widget=forms.TextInput()),
+    age = forms.IntegerField(widget=forms.NumberInput()),
+    weight = forms.FloatField(widget=forms.NumberInput()),
+    blood_type = forms.CharField(widget=forms.Select()),
+    health_condition = forms.CharField(widget=forms.Textarea()),
+    individual_type = 'R'
+
+    class Meta:
+        model = Recipient
+        fields = ('username', 'password', 'first_name', 'middle_name', 'last_name', 'contact_number', 'age', 'weight', 'blood_type', 'health_condition')
+
+    def __init__(self, *args, **kwargs):
+        super(RecipientForm, self).__init__(*args, **kwargs)
+        self.instance.type = self.type
+        self.instance.individual_type = self.individual_type
+
+
+class HospitalForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+    type = 'O'
+    name = forms.CharField(widget=forms.TextInput())
+    address = forms.CharField(widget=forms.TextInput())
+    contact_number = forms.CharField(widget=forms.TextInput())
+    org_type = 'H'
+
+    class Meta:
+        model = Hospital
+        fields = ['username', 'password', 'name', 'address', 'contact_number']
+
+    def __init__(self, *args, **kwargs):
+        super(HospitalForm, self).__init__(*args, **kwargs)
+        self.instance.type = self.type
+        self.instance.org_type = self.org_type
+
+
+class BloodBankForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+    type = 'O'
+    name = forms.CharField(widget=forms.TextInput())
+    address = forms.CharField(widget=forms.TextInput())
+    contact_number = forms.CharField(widget=forms.TextInput())
+    org_type = 'B'
+
+    class Meta:
+        model = BloodBank
+        fields = ['username', 'password', 'name', 'address', 'contact_number']
+
+    def __init__(self, *args, **kwargs):
+        super(BloodBankForm, self).__init__(*args, **kwargs)
+        self.instance.type = self.type
+        self.instance.org_type = self.org_type
+
+
+'''class IndividualForm(ModelForm):
     username = forms.CharField(widget=forms.TextInput)
     password = forms.CharField(widget=forms.PasswordInput)
     type = 'I'
@@ -13,9 +97,9 @@ class IndividualForm(ModelForm):
     age = forms.IntegerField(widget=forms.NumberInput)
     weight = forms.FloatField(widget=forms.FloatField)
     contact_number = forms.CharField(widget=forms.TextInput)
-    health_condition = models.CharField(widget=forms.Textarea)
-    blood_type = models.CharField(widget=forms.ComboField)
-    individual_type = models.CharField(widget=forms.ChoiceField)
+    health_condition = forms.CharField(widget=forms.Textarea)
+    blood_type = forms.CharField(widget=forms.Select)
+    individual_type = forms.CharField(widget=forms.Select)
 
     class Meta:
         model = Individual
@@ -30,11 +114,11 @@ class OrganizationForm(ModelForm):
     name = forms.CharField(widget=forms.TextInput)
     address = forms.CharField(widget=forms.TextInput)
     contact_number = forms.CharField(widget=forms.TextInput)
-    org_type = forms.CharField(widget=forms.ChoiceField)
+    org_type = forms.CharField(widget=forms.Select)
 
     class Meta:
         model = Organization
-        fields = ['username', 'password', 'name', 'address', 'contact_number', 'weight', 'contact_number', 'org_type']
+        fields = ['username', 'password', 'name', 'address', 'contact_number', 'contact_number', 'org_type']
 
 
 class BloodSupplyForm(ModelForm):
@@ -56,7 +140,7 @@ class BloodSupplyForm(ModelForm):
 class DonationForm(ModelForm):
     donor = forms.CharField(widget=forms.ChoiceField)
     blood_bank = forms.CharField(widget=forms.ChoiceField)
-    donation_date = models.DateField(widget=forms.DateField)
+    donation_date = forms.DateField(widget=forms.DateField)
     status = False
 
     class Meta:
@@ -65,9 +149,9 @@ class DonationForm(ModelForm):
 
 
 class TransfusionForm(ModelForm):
-    recipient = models.CharField(widget=forms.ChoiceField)
-    hospital = models.ForeignKey(widget=forms.ChoiceField)
-    transfusion_date = models.DateField(widget=forms.DateField)
+    recipient = forms.CharField(widget=forms.ChoiceField)
+    hospital = forms.ForeignKey(widget=forms.ChoiceField)
+    transfusion_date = forms.DateField(widget=forms.DateField)
     status = False
 
     class Meta:
@@ -76,11 +160,11 @@ class TransfusionForm(ModelForm):
 
 
 class RequestForm(ModelForm):
-    recipient = models.CharField(widget=forms.ChoiceField)
-    hospital = models.ForeignKey(widget=forms.ChoiceField)
-    transfusion_date = models.DateField(widget=forms.DateField)
+    recipient = forms.CharField(widget=forms.ChoiceField)
+    hospital = forms.ForeignKey(widget=forms.ChoiceField)
+    transfusion_date = forms.DateField(widget=forms.DateField)
     status = False
 
     class Meta:
         model = Transfusion
-        fields = ['recipient', 'hospital', 'transfusion_date']
+        fields = ['recipient', 'hospital', 'transfusion_date']'''
--- BloodBankSystem/accounts/views.py
+++ BloodBankSystem/accounts/views.py
@@ -1,3 +1,119 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.views import View
+from.models import User, Individual, Organization, Donor, Recipient, Hospital, BloodBank
+from .forms import DonorForm, RecipientForm, HospitalForm, BloodBankForm
 
-# Create your views here.
+class HomeView(View):
+    template = 'index.html'
+
+    def get(self, request):
+        if 'username' not in request.session:
+            return redirect(reverse('accounts:login'))
+        else:
+            return render(request, self.template)
+
+
+class LoginView(View):
+    template = 'login.html'
+
+    def get(self, request):
+        return render(request, self.template)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            if User.objects.get(username=username):
+                user = User.objects.get(username=username)
+                if user.password == password:
+                    request.session['username'] = user.username
+                    if user.type == 'I':
+                        individual = Individual.objects.get(username=user.username)
+                        request.session['first_name'] = individual.first_name
+                        request.session['last_name'] = individual.last_name
+                        request.session['type'] = individual.individual_type
+                    elif user.type == 'O':
+                        org = Organization.objects.get(username=user.username)
+                        request.session['name'] = org.name
+                        request.session['type'] = org.org_type
+                    return redirect(reverse('accounts:index'))
+        except User.DoesNotExist:
+            user = None
+        return render(request, self.template)
+
+
+class LogoutView(View):
+
+    def get(self, request):
+        request.session.clear()
+        return redirect(reverse('accounts:login'))
+
+
+class DonorRegistrationView(View):
+    template = 'register.html'
+
+    def get(self, request):
+        form = DonorForm()
+        context = {'form': form}
+        return render(request, self.template, context)
+    
+    def post(self, request):
+        form = DonorForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:login'))
+        return render(request, self.template, context)
+
+
+class RecipientRegistrationView(View):
+    template = 'register.html'
+
+    def get(self, request):
+        form = RecipientForm()
+        context = {'form': form}
+        return render(request, self.template, context)
+    
+    def post(self, request):
+        form = RecipientForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:login'))
+        return render(request, self.template, context)
+
+
+class HospitalRegistrationView(View):
+    template = 'register.html'
+
+    def get(self, request):
+        form = HospitalForm()
+        context = {'form': form}
+        return render(request, self.template, context)
+    
+    def post(self, request):
+        form = HospitalForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:login'))
+        return render(request, self.template, context)
+
+
+class BloodBankRegistrationView(View):
+    template = 'register.html'
+
+    def get(self, request):
+        form = BloodBankForm()
+        context = {'form': form}
+        return render(request, self.template, context)
+    
+    def post(self, request):
+        form = BloodBankForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:login'))
+        return render(request, self.template, context)
