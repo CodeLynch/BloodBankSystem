65b4481d71bd49f505c09e29df81f3d0e1552dd7
Vergil <88496530+CodeLynch@users.noreply.github.com>
2022-09-30 09:31:25 +0800

    Renaming Apps

---
+++ BloodBankSystem/accounts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+'''admin.site.register(User)
+admin.site.register(Individual)
+admin.site.register(Organization)
+admin.site.register(Donor)
+admin.site.register(Recipient)
+admin.site.register(Hospital)
+admin.site.register(BloodBank)'''
