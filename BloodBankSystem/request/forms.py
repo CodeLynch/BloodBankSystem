958fb4a1951b973ff7d89355f761c10400ca7179
jlambojo <114710482+jlambojo@users.noreply.github.com>
2022-10-13 01:38:34 +0800

    Addes CSS to Request Blood Supply

--- BloodBankSystem/request/forms.py
+++ BloodBankSystem/request/forms.py
@@ -7,16 +7,17 @@ max = 100
 min = 0
 
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
+# class DateInput(forms.DateInput):
+#     input_type = 'date'
 
 
 class RequestBloodSupplyForm(forms.ModelForm):
     #only include blood banks with blood supply in the choices
     blood_bank = forms.ModelChoiceField(widget=forms.Select(), queryset=BloodBank.objects.only('user_id').exclude(blood_supply_id=None))
-    blood_type = forms.CharField(widget=forms.Select()),
-    request_date = forms.DateField(widget=DateInput())
-    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'max': max, 'min': min}))
+    #blood_type = forms.CharField(widget=forms.Select()),
+    blood_type = forms.CharField(widget=forms.Select(choices = 'type_blood')),
+    request_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date', 'class':'form-control'}), label='Request Date')
+    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'max': max, 'min': min, 'class': 'form-control'}), label='Quantity')
 
     class Meta:
         model = Request
